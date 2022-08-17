6b6d9a342cf7bef09c39fee6cceb56eac3f63d6f
Joana <joanajanefdez@gmail.com>
2022-08-18 00:09:36 +0200

    controllers acabado

--- src/main/java/com/ud25_Cine/controller/PeliculasController.java
+++ src/main/java/com/ud25_Cine/controller/PeliculasController.java
@@ -1,5 +1,55 @@
 package com.ud25_Cine.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ud25_Cine.dto.Peliculas;
+import com.ud25_Cine.service.PeliculasServiceImpl;
+
+
+@RestController
+@RequestMapping("/api")
 public class PeliculasController {
 
+	@Autowired
+	PeliculasServiceImpl peliServImpl;
+	
+	@GetMapping("/peliculas")
+	public List<Peliculas> listPeliculas(){
+		return peliServImpl.listPeliculas();
+	}
+		
+	@GetMapping("/peliculas/{id}")
+	public Peliculas findById(@PathVariable(name="id") int id) {
+		return peliServImpl.findById(id);
+	}
+	
+	@DeleteMapping("/peliculas/{id}")
+	public void deletePeliculas(@PathVariable(name="id") int id) {
+		peliServImpl.deletePeliculas(id);
+	}
+	
+	@PostMapping("/peliculas")
+	public Peliculas savePeliculas(@RequestBody Peliculas pelicula) {
+		return peliServImpl.savePeliculas(pelicula);
+	}
+	
+	@PutMapping("/peliculas/{id}") //actualizar
+	public Peliculas updatePeliculas(@PathVariable(name="id") int id, @RequestBody Peliculas pelicula) {
+		Peliculas peli_selec = peliServImpl.findById(id);
+		
+		peli_selec.setNombre(pelicula.getNombre());
+		peli_selec.setCalif_edad(pelicula.getCalif_edad());
+				
+		return peliServImpl.savePeliculas(peli_selec);
+	}
 }
--- src/main/java/com/ud25_Cine/controller/SalasController.java
+++ src/main/java/com/ud25_Cine/controller/SalasController.java
@@ -1,5 +1,55 @@
 package com.ud25_Cine.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ud25_Cine.dto.Salas;
+import com.ud25_Cine.service.SalasServiceImpl;
+
+@RestController
+@RequestMapping("/api")
 public class SalasController {
+
+	@Autowired
+	SalasServiceImpl salasServImpl;
+	
+	@GetMapping("/salas")
+	public List<Salas> listSalas(){
+		return salasServImpl.listSalas();
+	}
+		
+	@GetMapping("/salas/{id}")
+	public Salas findById(@PathVariable(name="id") int id) {
+		return salasServImpl.findById(id);
+	}
+	
+	@DeleteMapping("/salas/{id}")
+	public void deleteSalas(@PathVariable(name="id") int id) {
+		salasServImpl.deleteSala(id);
+	}
+	
+	@PostMapping("/salas")
+	public Salas saveSalas(@RequestBody Salas sala) {
+		return salasServImpl.saveSala(sala);
+	}
+	
+	@PutMapping("/salas/{id}") //actualizar
+	public Salas updateSalas(@PathVariable(name="id") int id, @RequestBody Salas sala) {
+		Salas sala_selec = salasServImpl.findById(id);
+		
+		sala_selec.setNombre(sala.getNombre());
+		sala_selec.setPelicula(sala.getPelicula());
+				
+		return salasServImpl.saveSala(sala_selec);
+	}
 
 }
