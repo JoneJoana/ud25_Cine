19fd2f877052329149383bc0ab5b397c51a3eefa
Joana <joanajanefdez@gmail.com>
2022-08-17 22:09:10 +0200

    dto

--- src/main/java/com/ud25_Cine/dto/Peliculas.java
+++ src/main/java/com/ud25_Cine/dto/Peliculas.java
@@ -1,5 +1,75 @@
 package com.ud25_Cine.dto;
 
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name="peliculas")
 public class Peliculas {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String nombre;
+	private int calif_edad;
+	
+	@JsonIgnore
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "pelicula") //mappedBy la variable de Salas que hace ref a peliculas
+	private List<Salas> salas;
+	
+	public Peliculas() {}
+
+	public Peliculas(int id, String nombre, int calif_edad) {
+		this.id = id;
+		this.nombre = nombre;
+		this.calif_edad = calif_edad;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getCalif_edad() {
+		return calif_edad;
+	}
+
+	public void setCalif_edad(int calif_edad) {
+		this.calif_edad = calif_edad;
+	}
+
+	public List<Salas> getSalas() {
+		return salas;
+	}
+
+	public void setSalas(List<Salas> salas) {
+		this.salas = salas;
+	}
+
+	@Override
+	public String toString() {
+		return "Peliculas [id=" + id + ", nombre=" + nombre + ", calif_edad=" + calif_edad + "]";
+	}
+	
 }
--- src/main/java/com/ud25_Cine/dto/Salas.java
+++ src/main/java/com/ud25_Cine/dto/Salas.java
@@ -1,5 +1,62 @@
 package com.ud25_Cine.dto;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="salas")
 public class Salas {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String nombre;
+
+	@ManyToOne()
+	@JoinColumn(name="pelicula")
+	private Peliculas pelicula;
+	
+	public Salas() {}
+
+	public Salas(int id, String nombre, Peliculas pelicula) {
+		this.id = id;
+		this.nombre = nombre;
+		this.pelicula = pelicula;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Peliculas getPelicula() {
+		return pelicula;
+	}
+
+	public void setPelicula(Peliculas pelicula) {
+		this.pelicula = pelicula;
+	}
+
+	@Override
+	public String toString() {
+		return "Salas [id=" + id + ", nombre=" + nombre + ", pelicula=" + pelicula + "]";
+	}
 
 }
