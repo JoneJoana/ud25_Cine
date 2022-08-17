2d1e0972a5b1a27916e489b2155312e54808fb74
Joana <joanajanefdez@gmail.com>
2022-08-17 21:35:37 +0200

    init project

---
+++ src/main/java/com/ud25_Cine/Ud25CineApplication.java
@@ -0,0 +1,13 @@
+package com.ud25_Cine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ud25CineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ud25CineApplication.class, args);
+	}
+
+}
